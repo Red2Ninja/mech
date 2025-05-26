16fa864a486ea9435e95c15f0fa55d11fbdecabc
Red4Ninja <aryaanagvekar@gmail.com>
2025-05-26 14:36:25 +0530

    fixed responsiveness

--- src/components/pages/About.js
+++ src/components/pages/About.js
@@ -69,7 +69,7 @@ function AboutSection() {
           <div className='card-content-section'>
             <h2 className='card-title'>ABOUT ASME-VIT</h2>
             <p className='card-content'>
-              Mechnovate is the largest and widely recognized 3 day symposium conducted by ASME-VIT. It focuses on innovation and advancements in the field of mechanical engineering. It has successfully gathered inquisitive and talented minds from all over India. Our motto is to attain insights from our predecessors on the conduction of events and reinventing ourselves while implementing future technology in Mechnovate 2024. It includes workshops, keynote sessions and competitions to foster collaboration and exchange of ideas among students, researchers, and professionals in the domain of mechanical engineering. Mechnovate takes place over the course of 3 days packed with premium events.
+              We are a vibrant community of aspiring engineers dedicated to enhancing our skills and knowledge. Established in 2010 with the help of officials from ASME India, we have transpired to be one of the largest and the most distinguished student chapters directing our focus on stimulating innovation, fostering professional development, and networking opportunities. We impart a platform for students to explore their passion for mechanical engineering and make significant contributions to the field of mechanical engineering.
             </p>
           </div>
         </div>
