4dd5e1bd12c8241436720b968a8f4d802982c227
Red4Ninja <aryaanagvekar@gmail.com>
2025-06-11 17:31:01 +0530

    added loading spinner

--- src/components/HeroSection.js
+++ src/components/HeroSection.js
@@ -1,17 +1,31 @@
-import React from 'react'
+import React, { useState } from 'react';
 import './HeroSection.css';
 import '../App.css';
 
-
 function HeroSection() {
+  const [videoLoaded, setVideoLoaded] = useState(false);
+
   return (
     <section id="home">
-    <div className = 'hero-container'>
-        <video src='./videos/final.mp4' autoPlay loop muted></video>
-       
-        </div>
-        </section>
-  )
+      <div className="hero-container">
+        
+        {!videoLoaded && (
+          <div className="mascot-loader">
+            <img src="/assets/mascot.gif" alt="Loading..." />
+          </div>
+        )}
+
+        <video
+          src="./videos/final.mp4"
+          autoPlay
+          loop
+          muted
+          onCanPlayThrough={() => setVideoLoaded(true)}
+          className={videoLoaded ? 'visible' : 'hidden'}
+        ></video>
+      </div>
+    </section>
+  );
 }
 
 export default HeroSection;
